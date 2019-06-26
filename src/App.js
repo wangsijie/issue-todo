8c1792f3c978f879d6f00931428862a941da5c99
Wang Sijie <i@sijie.wang>
2019-06-26 08:54:30 +0800

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/home';
+import './App.less';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+		<div className="app">
+			<header className="app-header">
+			
+			</header>
+			<Router>
+				<Route path="/" exact component={Home} />
+			</Router>
+		</div>
+    );
 }
 
 export default App;
