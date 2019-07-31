3336276fb3cba3564413e078e32f62db0440289c
Wang Sijie <i@sijie.wang>
2019-07-31 15:18:19 +0800

    Updates

---
+++ precache-manifest.f5c9d447aaa7d730df7c2d946cf29725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b5c3d921758ab3fdadf47b9e318356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2b17ad3ccb0a5aa1dfd",
+    "url": "/static/css/2.9a43b543.chunk.css"
+  },
+  {
+    "revision": "e30ac57e55777d75f39f",
+    "url": "/static/css/main.6582200c.chunk.css"
+  },
+  {
+    "revision": "b2b17ad3ccb0a5aa1dfd",
+    "url": "/static/js/2.c81c8721.chunk.js"
+  },
+  {
+    "revision": "e30ac57e55777d75f39f",
+    "url": "/static/js/main.d2081a4e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
